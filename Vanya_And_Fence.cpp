4db469ead66ec6c6585fe8bcedd8a6991c5b45cf
Ayush Man Singh <ayushmans237@gmail.com>
2025-09-12 18:56:51 +0530

    Create Vanya_And_Fence.cpp

---
+++ Vanya_And_Fence.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n,h;
+    cin>>n>>h;
+    int a;
+
+    int count=0;
+    for(int i=0;i<n;i++){
+        cin>>a;
+        if(a<=h){
+            count++;
+        }
+        else{
+            count+=2;
+        }
+    }
+    cout<<count;
+    return 0;
+}
