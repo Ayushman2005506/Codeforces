96c82977759cb0b655eb27c3bdcc52825b1caf8c
Ayush Man Singh <ayushmans237@gmail.com>
2026-08-04 19:07:15 +0530

    Create Creating_Words.cpp
    Matthew is given two strings a
     and b
    , both of length 3
    . He thinks it's particularly funny to create two new words by swapping the first character of a
     with the first character of b
    . He wants you to output a
     and b
     after the swap.
    
    Note that the new words may not necessarily be different.
    
    Input
    The first line contains t
     (1≤t≤100
    )  — the number of test cases.
    
    The first and only line of each test case contains two space-separated strings, a
     and b
    , both of length 3
    . The strings only contain lowercase Latin letters.
    
    Output
    For each test case, after the swap, output a
     and b
    , separated by a space.

---
+++ Creating_Words.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    for(int i=0;i<t;i++){
+        string a,b;
+        cin>>a>>b;
+        swap(a[0],b[0]);
+        cout<<a<<" "<<b<<endl;
+    }
+    return 0;
+}
